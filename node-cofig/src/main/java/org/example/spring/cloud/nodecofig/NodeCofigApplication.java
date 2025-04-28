f97b980e537440d74a0374f05c409aa0b036f390
osman yaycioglu <osman.yaycioglu@gmail.com>
2025-04-28 15:42:42 +0300

    2

---
+++ node-cofig/src/main/java/org/example/spring/cloud/nodecofig/NodeCofigApplication.java
@@ -0,0 +1,16 @@
+package org.example.spring.cloud.nodecofig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class NodeCofigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeCofigApplication.class,
+                              args);
+    }
+
+}
---
+++ node-cofig/src/test/java/org/example/spring/cloud/nodecofig/NodeCofigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring.cloud.nodecofig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeCofigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
