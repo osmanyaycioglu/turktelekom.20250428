e1195ccd76bb80e991866cf72376af16999fdd5b
osman yaycioglu <osman.yaycioglu@gmail.com>
2025-04-29 11:54:37 +0300

    6

---
+++ ms-order/src/test/java/org/example/spring/cloud/msorder/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring.cloud.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
